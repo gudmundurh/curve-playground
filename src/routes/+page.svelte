8960a938079c68c5ad815ce419234d6a7c1ec1f3
Gudmundur Hreidarsson <gudmundur.hreidarsson@gmail.com>
2023-04-29 15:19:04 +0200

    Nicer viewport

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,5 @@
     const scene = new Scene();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <SvgRenderer {scene} />
+
--- src/routes/SvgRenderer.svelte
+++ src/routes/SvgRenderer.svelte
@@ -6,23 +6,52 @@
 	const colors = ['red', 'blue', 'green', 'magenta', 'orange', 'olive', 'brown'];
 </script>
 
-<input type="number" min="0" step="0.05" max="1" bind:value={t} />
+<div class="container">
+	<div class="controls">
+		t: <input type="number" min="0" step="0.05" max="1" bind:value={t} />
+	</div>
 
-<svg width="500" height="500" style:border="1px solid red">
-	<g transform="scale(3) translate(10, 100) matrix(1, 0, 0, -1, 0, 0)">
-		<line x1="-1000" x2="1000" y1="0" y2="0" stroke="black" stroke-width=".5" />
-		<line x1="0" x2="0" y1="-1000" y2="1000" stroke="black" stroke-width=".5" />
+	<svg width="100%" height="500">
+		<g transform="scale(3) translate(10, 100) matrix(1, 0, 0, -1, 0, 0)">
+			<line x1="-1000" x2="1000" y1="0" y2="0" stroke="black" stroke-width=".5" />
+			<line x1="0" x2="0" y1="-1000" y2="1000" stroke="black" stroke-width=".5" />
 
-		{#each scene.objects as object, i}
-			{@const x = typeof object === 'function' ? object(t).x : object.x}
-			{@const y = typeof object === 'function' ? object(t).y : object.y}
+			{#each scene.objects as object, i}
+				{@const x = typeof object === 'function' ? object(t).x : object.x}
+				{@const y = typeof object === 'function' ? object(t).y : object.y}
 
-			<circle cx={x} cy={y} r="2" fill={colors[i % colors.length]} />
-			<g transform="matrix(1, 0, 0, -1, 0, 0)">
-				<text {x} y={-y} dx={2} dy={-2} fill={colors[i % colors.length]} style="font-size: 0.5em"
-					>{object.label}</text
-				>
-			</g>
-		{/each}
-	</g>
-</svg>
+				<circle cx={x} cy={y} r="2" fill={colors[i % colors.length]} />
+				<g transform="matrix(1, 0, 0, -1, 0, 0)">
+					<text {x} y={-y} dx={2} dy={-2} fill={colors[i % colors.length]} style="font-size: 0.5em"
+						>{object.label}</text
+					>
+				</g>
+			{/each}
+		</g>
+	</svg>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+	}
+
+	.controls {
+		background: #333;
+		color: #fff;
+		padding: 1rem;
+		flex-grow: 0;
+	}
+
+	.controls input {
+		background: #444;
+		border: 1px solid #999;
+		color: #fff;
+	}
+
+	svg {
+		flex: 1;
+	}
+</style>
