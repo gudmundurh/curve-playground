51a999d19921744a97d9ee1af5866b5cf2a5301e
Gudmundur Hreidarsson <gudmundur.hreidarsson@gmail.com>
2023-06-20 19:39:58 +0200

    Scene selector

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-	import SvgRenderer from "./SvgRenderer.svelte";
-	import { CubicScene, QuadraticScene  } from "./scenes";
-    
-    const scene = new CubicScene();
-    //const scene = new QuadraticScene();
+	import SvgRenderer from './SvgRenderer.svelte';
+	import { CubicScene, QuadraticScene } from './scenes';
+	import type { Scene } from './shapes';
+
+	let scene: Scene = new CubicScene();
 </script>
 
 <svelte:head>
-    <title>Curve Playground</title>
+	<title>Curve Playground</title>
 </svelte:head>
 
-<SvgRenderer {scene} />
-
-    
+<div class="with-sidebar">
+	<div class="sidebar stack">
+		<h2>Scene</h2>
+		<button on:click={() => (scene = new CubicScene())}>Cubic</button>
+		<button on:click={() => (scene = new QuadraticScene())}>Quadratic</button>
+	</div>
+	<SvgRenderer {scene} />
+</div>
